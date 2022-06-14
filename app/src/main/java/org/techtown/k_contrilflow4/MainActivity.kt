e53c590f6358ed78d0bc2ddf1498518360683334
Bongsic2 <96456795+Bongsic2@users.noreply.github.com>
2022-06-15 06:09:57 +0900

    K_ControlFlow4

---
+++ app/src/main/java/org/techtown/k_contrilflow4/MainActivity.kt
@@ -0,0 +1,22 @@
+package org.techtown.k_contrilflow4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var now = 9
+        when (now) {
+            8, 9 -> {
+                Log.d("when", "현재 시간은 8시 또는 9시입니다.")
+            }
+            else -> {
+                Log.d("when", "현재 시간은 9시가 아닙니다.")
+            }
+        }
+    }
+}
